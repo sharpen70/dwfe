2f4ade7233e28a76fef40d25d05cd84e3750bedd
PENG XIAO <sharpen70@example.com>
2018-10-10 16:59:42 +1000

    minor changes

--- src/main/java/org/guiiis/dwfe/App.java
+++ src/main/java/org/guiiis/dwfe/App.java
@@ -5,13 +5,20 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
+import org.guiiis.dwfe.core.RewritingAlgorithm;
 import org.guiiis.dwfe.utils.SimpleQueryFileReader;
 
 import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
 import fr.lirmm.graphik.graal.api.core.RuleSet;
+import fr.lirmm.graphik.graal.api.core.RulesCompilation;
 import fr.lirmm.graphik.graal.api.kb.KnowledgeBase;
+import fr.lirmm.graphik.graal.backward_chaining.pure.AggregSingleRuleOperator;
+import fr.lirmm.graphik.graal.core.compilation.IDCompilation;
+import fr.lirmm.graphik.graal.core.compilation.NoCompilation;
+import fr.lirmm.graphik.graal.core.ruleset.IndexedByHeadPredicatesRuleSet;
 import fr.lirmm.graphik.graal.io.dlp.DlgpParser;
 import fr.lirmm.graphik.graal.kb.KBBuilder;
+import fr.lirmm.graphik.util.profiler.Profiler;
 import fr.lirmm.graphik.util.profiler.RealTimeProfiler;
 
 /**
@@ -20,23 +27,39 @@ import fr.lirmm.graphik.util.profiler.RealTimeProfiler;
  */
 public class App 
 {
+	private static final String DLGFLAG = "dlg";
+	private static final String UCQFLAG = "ucq";
+	
 	public static void main(String[] args) throws Exception {
 		PrintStream output = System.out;
 		
-		if(args.length < 1) { System.out.println("Missing Ontology File !!"); return; }
-		if(args.length < 2) { System.out.println("Missing Queries File !!"); return; }
-		if(args.length >= 3) { 
-			File o = new File(args[2]);
+		String ontologyfile = null;
+		String queriesfile = null;
+		String outputfile = null;
+		
+		String mode = DLGFLAG;
+		
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].startsWith("-")) {
+				if(args[i].equals("-o")) outputfile = args[++i];
+				else mode = args[i].substring(1);
+			}
+			else {
+				if(ontologyfile == null) ontologyfile = args[i];
+				else queriesfile = args[i];
+			}
+		}
+		
+		if(ontologyfile == null) { System.out.println("Missing Ontology File !!"); return; }
+		if(queriesfile == null) { System.out.println("Missing Queries File !!"); return; }
+		if(outputfile != null) { 
+			File o = new File(outputfile);
 			o.getParentFile().mkdirs();
 			o.createNewFile();
 			output = new PrintStream(new FileOutputStream(o, false)); 
 		}
 		
-		String ontologyfile = args[0];
-		String queriesfile = args[1];
-		
-		output.println("Evaluating ontology " + ontologyfile + " and queries " + queriesfile);
-		// 0 - Create a KBBuilder
+    	// 0 - Create a KBBuilder
 		KBBuilder kbb = new KBBuilder();
 	
 		kbb.addAll(new DlgpParser(new File(ontologyfile)));
@@ -46,16 +69,31 @@ public class App
 		RuleSet ontology = kb.getOntology();
 		
 		ArrayList<String> qs = SimpleQueryFileReader.read(new File(queriesfile));
-		
-		DatalogRewriting dr = new DatalogRewriting();
-		
-		dr.setProfiler(new RealTimeProfiler(output));
+
 		
 		for(String s : qs) {
 //			System.out.println(s);
 			ConjunctiveQuery query = DlgpParser.parseQuery(s);
-			output.println(query.toString());
-			dr.exec(query, ontology);			
+			if(mode.equals(DLGFLAG)) {
+				DatalogRewriting dr = new DatalogRewriting();			
+				dr.setProfiler(new RealTimeProfiler(output));
+				output.println(query.toString());
+				dr.exec(query, ontology);	
+			}
+			if(mode.equals(UCQFLAG)) {
+				IndexedByHeadPredicatesRuleSet indexedRuleSet = new IndexedByHeadPredicatesRuleSet(ontology);
+
+				// rewriting
+				RulesCompilation compilation = new IDCompilation();
+//				compilation.compile(ontology.iterator());
+//				
+				AggregSingleRuleOperator operator = new AggregSingleRuleOperator();
+				Profiler p = new RealTimeProfiler(output);
+				RewritingAlgorithm algo = new RewritingAlgorithm(operator);
+				operator.setProfiler(p);
+				algo.setProfiler(p);
+				algo.execute(query, indexedRuleSet, NoCompilation.instance());
+			}
 		}
 		
 //		ConjunctiveQuery query = DlgpParser.parseQuery(qs.get(3));
